1af727deba13476072455ccdd45ffeb8a3ccf031
lulin <linlinlu77@163.com>
2021-06-21 16:38:24 +0800

    展示数据

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-06-19 17:45:46
+ * @LastEditTime: 2021-06-21 16:21:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
  */
 
 // 创建“外壳”组件APP
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import Search from './components/Search'
 import List from './components/List'
-export default class APP extends Component{
+export default class APP extends Component {
+  // 初始化状态
+  state = { users: [] }
+  saveUsers = (users) => {
+    this.setState({ users })
+  }
   render() {
-    return(
+    return (
       <div>
-        <Search/>
-        <List/>
+        <Search saveUsers={this.saveUsers}/>
+        <List users={this.state.users} />
       </div>
     )
   }
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
@@ -5,14 +6,22 @@ import './index.css';
 
 export default class List extends Component {
   render() {
+    const {users} = this.props
     return (
-      <div>
-       <div className="card">
-								<a  href="https://github.com/reactjs" target="_blank">
-									<img alt="head_portrait" style={{width:'100px'}}/>
+      <div className='row'>
+        {
+        users.map((userObj)=> {
+          return(
+            <div key={userObj.id} className="card">
+								<a  href={userObj.html_url} target="_blank">
+									<img alt="head_portrait" src={userObj.avatar_url} style={{width:'100px'}}/>
 								</a>
-								<p className="card-text">123</p>
-							</div>
+								<p className="card-text">{userObj.login}</p>
+						</div>
+          ) 
+        })
+      }
+      
       </div>
     )
   }
