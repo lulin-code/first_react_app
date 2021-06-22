0bbfe301701cf94b898053317b4dcb5e8ae66ff4
lulin <linlinlu77@163.com>
2021-06-22 16:38:50 +0800

    封装NavLink组件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-06-22 15:53:17
+ * @LastEditTime: 2021-06-22 16:37:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
@@ -13,6 +13,7 @@ import {  NavLink,Route } from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 import Header from './components/Header'
+import MyNavLink from './components/MyNavLink'
 export default class APP extends Component{
   render() {
     return (
@@ -31,9 +32,8 @@ export default class APP extends Component{
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
               {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-              <NavLink activeClassName="activeL" className="list-group-item" to="/home">Home</NavLink>
-              <NavLink activeClassName="activeL"  className="list-group-item" to="/about">About</NavLink>
-              
+              <MyNavLink to="home"> Home</MyNavLink>
+              <MyNavLink to="about">About </MyNavLink>
             </div>
           </div>
           <div className="col-xs-6">
---
+++ src/components/MyNavLink/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom';
+
+export default class MyNavLink extends Component {
+  render() {
+    return (
+      <div>
+        <NavLink activeClassName="activeL" className="list-group-item" {...this.props}/>
+      </div>
+    )
+  }
+}
+
