1c523fe12c43d995f5d45dc43c6cce915f0c9846
lulin <linlinlu77@163.com>
2021-06-22 15:16:14 +0800

    路由的基本使用

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-06-17 14:25:51
+ * @LastEditTime: 2021-06-22 14:18:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
@@ -9,14 +9,41 @@
 
 // 创建“外壳”组件APP
 import React,{Component} from 'react'
-import Hello from './components/Hello/Hello.jsx'
-import Welocme from './components/Welcome/Welcome.jsx'
+import { Link,Route } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
 export default class APP extends Component{
   render() {
-    return(
+    return (
       <div>
-        <Hello/>
-        <Welocme/>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+
+              {/* 原生html中，靠<a>跳转不同的页面 */}
+              {/* <a className="list-group-item" href="./about.html">About</a>
+							<a className="list-group-item active" href="./home.html">Home</a> */}
+
+              {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
+              <Link className="list-group-item" to="/about">About</Link>
+              <Link className="list-group-item" to="/home">Home</Link>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Route path="/about" component={About} />
+                <Route path="/home" component={Home} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-06-17 11:18:47
+ * @LastEditTime: 2021-06-22 14:47:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/index.js
@@ -10,13 +10,16 @@
 import React from 'react';
 // 引入reactDOM
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter >,
   </React.StrictMode>,
   document.getElementById('root')
 );
