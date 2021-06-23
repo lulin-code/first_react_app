609702dd6f0b27f93f81101f9112a8b19dc4f849
lulin <linlinlu77@163.com>
2021-06-23 11:03:27 +0800

    嵌套路由

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import MyNavLink from '../../components/MyNavLink'
+import { Route,Switch } from 'react-router-dom'
+import Message from './Message'
+import News from './News'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <h3>我是Home的内容</h3>
+        	<ul className="nav nav-tabs">
+							<li>
+								<MyNavLink to="/home/news">News</MyNavLink>
+							</li>
+							<li>
+								<MyNavLink to="/home/message">Message</MyNavLink>
+							</li>
+						</ul>
+            <Switch>
+              <Route path="/home/news" component={News}></Route>
+              <Route path="/home/message" component={Message}></Route>
+            </Switch>
       </div>
     )
   }
