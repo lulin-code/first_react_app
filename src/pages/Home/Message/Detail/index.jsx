c6004d5ca88929ce373546bb1964d8a0c133061e
lulin <linlinlu77@163.com>
2021-06-24 11:36:18 +0800

    向路由组件传递state参数

--- src/pages/Home/Message/Detail/index.jsx
+++ src/pages/Home/Message/Detail/index.jsx
@@ -7,12 +7,10 @@ export default class Detail extends Component {
         {id:'02',content:'你好，尚硅谷'},
         {id:'03',content:'你好，未来的自己'}
     ]
-    const {search} = this.props.location
-    const {id,title} = qs.parse(search.slice(1))
-    console.log(qs.parse(search))
+    const {id,title} = this.props.location.state || {}
     const findResult = DetailData.find((detailObj)=>{
       return detailObj.id === id
-    })
+    }) || {}
     return (
       <div>
         <ul>
--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -23,7 +23,9 @@ export default class Message extends Component {
                     {/* 向路由组件传递params参数 */}
 					        	{/* <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link> */}
                     {/* 向路由组件传递search参数 */}
-                    <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link>
+                    {/* <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link> */}
+                    {/* 向路由组件传递state参数 */}
+                    <Link to={{pathname:'/home/message/detail',state:{id:msgObj.id,title:msgObj.title}}}>{msgObj.title}</Link>
 					        </li>
               )
             })
@@ -33,7 +35,9 @@ export default class Message extends Component {
         {/* 声明接收params参数 */}
         {/* <Route path="/home/message/detail/:id/:title" component={Detail}/> */}
         {/* search参数无需声明接收，正常注册路由即可 */}
-        <Route path="/home/message/detail" component={Detail}/>
+        {/* <Route path="/home/message/detail" component={Detail}/> */}
+        {/* state参数无需声明接收，正常注册路由即可 */}
+        <Route path="/home/message/detail" component={Detail}></Route>
       </div>
     )
   }
