eb27dfc1c648761141804a64fcfa92f8bbe67a71
lulin <linlinlu77@163.com>
2021-06-24 15:37:25 +0800

    空白项目

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-06-17 14:25:51
+ * @LastEditTime: 2021-06-24 15:37:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
@@ -9,14 +9,12 @@
 
 // 创建“外壳”组件APP
 import React,{Component} from 'react'
-import Hello from './components/Hello/Hello.jsx'
-import Welocme from './components/Welcome/Welcome.jsx'
+
 export default class APP extends Component{
   render() {
     return(
       <div>
-        <Hello/>
-        <Welocme/>
+        APP.......
       </div>
     )
   }
