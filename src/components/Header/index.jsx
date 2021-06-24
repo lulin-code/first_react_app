1fd582e0d4f1aac589a61e5d6e9af7a429a5392f
lulin <linlinlu77@163.com>
2021-06-24 15:09:43 +0800

    withRouter的使用

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
 
-export default class Header extends Component {
+ class Header extends Component {
+   back = ()=>{
+     this.props.history.goBack()
+   }
+   forward = ()=> {
+    this.props.history.goForward()
+   }
+   go = ()=> {
+     this.props.history.go(-2)
+   }
   render() {
     return (
       <div>
          <div className="page-header"><h2>React Router Demo</h2></div>
+         	<button onClick={this.back}>回退</button>&nbsp;
+				<button onClick={this.forward}>前进</button>&nbsp;
+				<button onClick={this.go}>go</button>
       </div>
     )
   }
 }
+export default withRouter(Header)
