b0ee3aa4307020bf26a64da009c76ae03dbf32e8
lulin <linlinlu77@163.com>
2021-06-25 11:05:01 +0800

    纯react版本求和案例

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-06-24 15:37:02
+ * @LastEditTime: 2021-06-25 10:49:57
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
@@ -9,12 +9,13 @@
 
 // 创建“外壳”组件APP
 import React,{Component} from 'react'
+import Count from './components/Count'
 
 export default class APP extends Component{
   render() {
     return(
       <div>
-        APP.......
+        <Count/>
       </div>
     )
   }
