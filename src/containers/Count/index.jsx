db062c0d4ae7a2008cbc7b8e9df4e31c9e658451
lulin <linlinlu77@163.com>
2021-06-30 17:18:53 +0800

    简写mapDispath

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -6,27 +6,10 @@ import { createIncrementAction,
          createIncrementAsyncAction,
          createDecrementAction } from '../../redux/count_action'
 
-/*
-  1.mapStateToProps函数返回的是一个对象
-  2.返回的对象中的key作为传递给UI组件props的key,value就作为传递给UI组件props的value
-  3.mapStateToProps用于传递状态
-*/ 
-const mapStateToProps = (state) => {
-  return {
-    count: state
-  }
-}
-/*
-  1.mapDispatchToProps函数返回的是一个对象
-  2.返回的对象中的key作为传递给UI组件props的key,value就作为传递给UI组件props的value
-  3.mapDispatchToProps用于传递操作状态的方法
-*/ 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    jia:number => dispatch(createIncrementAction(number)),
-    jian:number => dispatch(createDecrementAction(number)),
-    jiaAsync: (number,time)=>dispatch(createIncrementAsyncAction(number,time))
-  }
-}
 // 使用connect()()创建并暴露一个Count的容器组件
-export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
+export default connect(
+  state =>  ({count: state}),
+  { jia:createIncrementAction,
+    jian:createDecrementAction,
+    jiaAsync: createIncrementAsyncAction}
+  )(CountUI)
