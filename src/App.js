da16e60e05a59fd87d7632d8e5daaa61f483f6a1
lulin <linlinlu77@163.com>
2021-07-01 18:29:40 +0800

    扩展2_lazyLoad

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-07-01 17:51:07
+ * @LastEditTime: 2021-07-01 18:27:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
  */
 
 // 创建“外壳”组件APP
-import React,{Component} from 'react'
-import Demo from './components/1_setState/index'
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Demo from './components/2_lazyLoad/index.jsx'
 
-export default class APP extends Component{
+export default class APP extends Component {
   render() {
-    return(
+    return (
       <div>
-        <Demo/>
+        <BrowserRouter>
+          <Demo />
+        </BrowserRouter>
       </div>
     )
   }
