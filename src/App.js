175dbd50d3f04bc39b8ea1d2503207493b46ae86
lulin <linlinlu77@163.com>
2021-07-01 17:58:29 +0800

    扩展setState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-06-24 15:37:02
+ * @LastEditTime: 2021-07-01 17:51:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
@@ -9,12 +9,13 @@
 
 // 创建“外壳”组件APP
 import React,{Component} from 'react'
+import Demo from './components/1_setState/index'
 
 export default class APP extends Component{
   render() {
     return(
       <div>
-        APP.......
+        <Demo/>
       </div>
     )
   }
