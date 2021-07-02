bbbb3e0bff3c09ffba6d7a874a15e8b26d384f43
lulin <linlinlu77@163.com>
2021-07-02 15:33:32 +0800

    扩展7_context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-07-02 14:57:56
+ * @LastEditTime: 2021-07-02 15:14:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
@@ -10,7 +10,7 @@
 // 创建“外壳”组件APP
 import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import Demo from './components/4_Fragment'
+import Demo from './components/5_Context'
 
 export default class APP extends Component {
   render() {
