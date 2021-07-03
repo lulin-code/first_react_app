c392e1ad16a9c49a8cf0aa5c9cf9135c50847a39
lulin <linlinlu77@163.com>
2021-07-03 13:41:48 +0800

    拓展8_optimize

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 19:25:58
- * @LastEditTime: 2021-07-02 15:14:19
+ * @LastEditTime: 2021-07-03 13:38:03
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /first_react_app/src/App.js
@@ -10,7 +10,7 @@
 // 创建“外壳”组件APP
 import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import Demo from './components/5_Context'
+import Demo from './components/6_optimize'
 
 export default class APP extends Component {
   render() {
